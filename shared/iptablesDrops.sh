f5b7ebca6fd032b3aa6ea68cf750808964e48337
Rémi Lhuissier <remi.lhuissier@etu.unilim.fr>
2023-11-16 10:55:40 +0100

    add serveur s + addresses ip

--- shared/iptablesDrops.sh
+++ shared/iptablesDrops.sh
@@ -1,3 +1,4 @@
-iptables -P INPUT DROP
-iptables -P OUTPUT DROP
-iptables -A INPUT -p icmp -j ACCEPT
+#iptables -P INPUT DROP
+#iptables -P OUTPUT DROP
+#iptables -A INPUT -p icmp -j ACCEPT
+#iptables -A OUTPUT -p icmp -j ACCEPT
