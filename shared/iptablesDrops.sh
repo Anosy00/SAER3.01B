6d29a4f7fe6d7148dc675388e7ae506dd6ae9d40
Lucas <lucas.theodore2004@gmail.com>
2024-01-12 11:12:17 +0100

    Ending sae

--- shared/iptablesDrops.sh
+++ shared/iptablesDrops.sh
@@ -1,2 +1,6 @@
-iptables -P INPUT DROP
-iptables -P OUTPUT DROP
+iptables -A FORWARD -p udp --dport 53 -j ACCEPT
+iptables -A FORWARD -p udp --sport 53 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 53 -j ACCEPT
+iptables -A FORWARD -p tcp --sport 53 -j ACCEPT
+iptables -A FORWARD -p tcp -m multiport --sports 443,80 -j ACCEPT
+iptables -A FORWARD -p tcp -m multiport --dports 443,80 -j ACCEPT
