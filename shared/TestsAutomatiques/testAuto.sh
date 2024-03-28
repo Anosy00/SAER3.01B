f65ba331cf4235b405c8f9e933db3e837730407d
Rémi Lhuissier <remi.lhuissier@etu.unilim.fr>
2024-03-28 19:01:14 +0100

    bug fixed execution of testAuto.sh

--- shared/TestsAutomatiques/testAuto.sh
+++ shared/TestsAutomatiques/testAuto.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
 nping 172.12.150.1 -tcp -p 80 | grep "RCVD" > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-	echo "Connection from r0 to S by 80 successed"
+    echo "Connection from r0 to S by 80 succeeded"
 else
-	echo "Connection from r0 to S by 80 failed"
+    echo "Connection from r0 to S by 80 failed"
 fi
+
+exit 0
