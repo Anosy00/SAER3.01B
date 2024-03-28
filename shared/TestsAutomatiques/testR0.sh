56f7bfbe1edaaafaa0673ea0d5a6f5924d6bb716
Rémi Lhuissier <remi.lhuissier@etu.unilim.fr>
2024-03-28 14:37:46 +0100

    automated script skeleton

--- shared/TestsAutomatiques/testR0.sh
+++ shared/TestsAutomatiques/testR0.sh
@@ -12,18 +12,28 @@ test_ping_port() {
 
 # Configuration des composants Kathara
 declare -A kathara_nodes=(
-    [Google]="8.8.8.8:443"
-    [Google]="8.8.8.8:80"
+    [Google]="8.8.8.8,443,80"
+    [OpenAI]="45.33.121.77,80,443,22"
     # Ajoutez d'autres nœuds Kathara si nécessaire
 )
 
-# Boucle à travers les nœuds Kathara pour tester le ping
+# Boucle à travers les nœuds Kathara pour tester le ping sur les ports spécifiés
 for hostname in "${!kathara_nodes[@]}"; do
-    ip_port="${kathara_nodes[$hostname]}"
-    ip="${ip_port%:*}"
-    port="${ip_port#*:}"
-    echo "Test de connectivité vers $hostname ($ip):($port)"
-    test_ping_port "$ip" "$port"
-    echo
-done
+    echo ''
+    ip_ports="${kathara_nodes[$hostname]}"
+    IFS=',' read -r -a ip_ports_array <<< "$ip_ports"
+    ip="${ip_ports_array[0]}"
+    ports_to_test=("${ip_ports_array[@]:1}")
 
+    # Création de la chaîne contenant tous les ports à tester
+    ports_string=$(IFS=','; echo "${ports_to_test[*]}")
+
+    # Affichage du message avec tous les ports à tester
+    echo "Test de connectivité vers $hostname ($ip) sur les ports : $ports_string"
+
+    # Boucle à travers les ports à tester
+    for port in "${ports_to_test[@]}"; do
+        test_ping_port "$ip" "$port"
+        
+    done
+done
